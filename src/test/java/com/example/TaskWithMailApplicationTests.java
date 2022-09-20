574d636e247f4569fe095e254b8537dd7eb8302f
GeorgeWahson <georgewahson@qq.com>
2022-09-20 14:49:35 +0800

    测试发送单条邮件

--- src/test/java/com/example/TaskWithMailApplicationTests.java
+++ src/test/java/com/example/TaskWithMailApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import com.example.service.SendMailService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskWithMailApplicationTests {
 
+    @Autowired
+    private SendMailService service;
+
     @Test
-    void contextLoads() {
+    void testSendSimpleMessage() {
+        service.sendSimpleMail();
+    }
+
+    @Test
+    void testSendComponentMessage() {
+        service.sendComponentMail();
     }
 
 }
